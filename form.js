105547a69b0f7cdb67c95f09cbed87602473bd4c
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2022-06-06 17:38:27 +0530

    Changed class name ProcessInput to Form   extracted messages in Form

--- form.js
+++ form.js
@@ -1,24 +1,13 @@
 const fs = require('fs');
-const { ProcessInput } = require('./processInput.js');
+const { Form } = require('./processInput.js');
 
 
-const readInput = (callBack, writeInFile) => {
-  const messages = ['Please enter your name', 'Enter your dob (yyyy-mm-dd)', 'Enter your hobbies', 'thank you'];
-  let index = 0;
-
+const readInput = (callBack) => {
   process.stdin.setEncoding('utf8');
-  console.log(messages[index]);
 
   process.stdin.on('data', (chunk) => {
     callBack(chunk.slice(0, -1));
-    index++;
-    console.log(messages[index]);
-    if (index > 2) {
-      writeInJson(writeInFile);
-      process.exit(0);
-    }
-  })
-
+  });
 };
 
 const writeInJson = data => {
@@ -26,9 +15,17 @@ const writeInJson = data => {
 }
 
 const form = () => {
-  const processInput = new ProcessInput();
+  const form = new Form();
 
-  readInput((chunk) => processInput.addChunk(chunk), processInput.toString());
+  form.message();
+  readInput((chunk) => {
+    form.addChunk(chunk);
+    form.message();
+    if (form.allInputReceived()) {
+      writeInJson(form.toString());
+      process.exit(0);
+    }
+  });
 }
 
 
--- processInput.js
+++ processInput.js
@@ -1,24 +1,33 @@
-class ProcessInput {
+class Form {
   constructor() {
     this.userInfo = {};
     this.keys = ['name', 'dob', 'hobbies'];
+    this.messages = ['Please enter your name', 'Enter your dob (yyyy-mm-dd)', 'Enter your hobbies'];
     this.index = 0;
   }
 
   addChunk(chunk) {
     const key = this.keys[this.index];
     if (key === 'hobbies') {
-      this.userInfo[key] = [(chunk)]
+      this.userInfo[key] = [(chunk)];
     } else {
       this.userInfo[key] = chunk;
     }
     this.index++;
   }
 
+  message() {
+    console.log(this.messages[this.index]);
+  }
+
   toString() {
     return this.userInfo;
   }
 
+  allInputReceived() {
+    return this.index === 3;
+  }
+
 };
 
-exports.ProcessInput = ProcessInput;
+exports.Form = Form;
